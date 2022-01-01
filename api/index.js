6f25a6a773f35ce1a115bde5458340d580342206
JuniYadi <juniyadi94@gmail.com>
2022-01-01 15:11:55 +0700

    Allow CORS

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 const app = require('express')();
 const WhichBrowser = require('which-browser');
 
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
 app.get('/api/browser', (req, res) => {
     const useragent = req.query['ua'];
     const browser = new WhichBrowser(useragent);
@@ -14,4 +31,4 @@ app.get('/api/browser', (req, res) => {
     });
 })
 
-module.exports = app;
+module.exports = allowCors(app);
